ef1c2666e41843da84efe654a9c01152b160333b
Maj Lenaršič <lenarsicmaj@gmail.com>
2018-08-24 12:34:47 +0200

    load logged in user fix and login required implementation

--- admin.py
+++ admin.py
@@ -9,18 +9,11 @@ app = Blueprint('admin', __name__)
 
 @app.before_app_request
 def load_logged_in_user():
-    db = database.dbcon()
-    cur = db.cursor()
     user_id = session.get('user_id')
-
     if user_id is None:
         g.user = None
     else:
-
-        cur.execute(
-            """SELECT * FROM user WHERE id = %s""", (user_id,)
-        )
-        g.user = cur.fetchone()
+        g.user = session
 
 @app.route('/login', methods=('GET', 'POST'))
 def login():
@@ -41,7 +34,8 @@ def login():
 
         if error is None:
             session.clear()
-            session['user_id'] = user[0]
+            session['user_id'] = int(user[0])
+            session['username'] = user[1]
             return render_template('admin.riddle.html')
 
         print(error)
@@ -51,23 +45,25 @@ def login():
 @app.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for('admin.login'))
 
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
-            return redirect(url_for('auth.login'))
+            return redirect(url_for('admin.login'))
 
         return view(**kwargs)
 
     return wrapped_view
 
 @app.route('/riddle')
+@login_required
 def riddle():
     return render_template('admin.riddle.html')
 
 @app.route('/substitution')
+@login_required
 def substitution():
     return render_template('admin.substitution.html')
 
